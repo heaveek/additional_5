106eb1e5b26f6179ae99767fe903e33d2ccbaae2
heaveek <heaveek@gmail.com>
2018-09-26 14:13:15 +0300

    16 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arrStr = str.split('');
+  if (arrStr.length % 2 !== 0) {
+    return false;
+  }
+
+  for (let i = 0; i <= arrStr.length; i++) {
+    for (let j = 0; j <= arrStr.length; j++) {
+      if(arrStr[i] == bracketsConfig[i][j]) {        
+          return true;
+      } else {return false;}
+    }
+  } 
 }
